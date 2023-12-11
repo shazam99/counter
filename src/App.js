3e865b7257bb99a20b1f7caec194dd43ac5f0239
Siddharth <sharmazsiddharth@gmail.com>
2023-12-11 21:37:55 +0530

    Button name changed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  
+
   const [count,setCount] = useState(0);
 
   return (
@@ -10,8 +10,8 @@ function App() {
       <h1>Counter App</h1>
       <hr />
       <h4>Count: {count}</h4>
-      <button className="inc" onClick={()=>setCount(count+1)}>Increase Count</button>
-      <button className="dec" onClick={()=>setCount(count-1)}>Decrease Count</button>
+      <button className="inc" onClick={()=>setCount(count+1)}>Increment</button>
+      <button className="dec" onClick={()=>setCount(count-1)}>Decrement</button>
     </div>
   );
 }
